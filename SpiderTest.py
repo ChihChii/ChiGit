54914076fdfaea0fb85c33854fa3825c0aba75e2
ChihChii <chihchi0209@gmail.com>
2019-06-18 00:48:40 +0800

    0618 update

--- SpiderTest.py
+++ SpiderTest.py
@@ -7,7 +7,12 @@ Created on Mon Jun 10 17:51:26 2019
 import requests
 import time
 from bs4 import BeautifulSoup
+import os
+import re
+import urllib.request
+import json
 
+PTT_URL = 'https://www.ptt.cc'
 
 def get_web_page(url):
     time.sleep(0.5)
@@ -24,6 +29,9 @@ def get_web_page(url):
     
 def get_articles(dom, date):
     soup = BeautifulSoup(dom, 'html.parser')
+    
+    paging_div = soup.find('div', 'btn-group btn-group-paging')
+    prev_url = paging_div.find_all('a')[1]['href']
     
     articles = []
     divs = soup.find_all('div','r-ent')
@@ -44,12 +52,56 @@ def get_articles(dom, date):
                         'title':title,
                         'href' : href,
                         'push_count' : push_count})
-    return articles
+    return articles, prev_url
 
 
-page = get_web_page('https://www.ptt.cc/bbs/Beauty/index.html')
-if page :
-    date = time.strftime("%m%d").lstrip('0')
-    current_articles = get_articles(page, date)
-    for post in current_articles:
-        print(post)
+
+def parse(dom):
+    soup = BeautifulSoup(dom, 'html.parser')
+    links = soup.find(id='main-content').find_all('a')
+    img_urls = []
+    for link in links:
+        if re.match(r'^https?://(i.)?(m.)?imgur.com', link['href']):
+            img_urls.append(link['href'])
+    return img_urls
+
+
+def save(img_urls, title):
+    if img_urls:
+        try:
+            dname = title.strip()  
+            os.makedirs(dname)
+            for img_url in img_urls:
+                if img_url.split('//')[1].startswith('m.'):
+                    img_url = img_url.replace('//m.', '//i.')
+                if not img_url.split('//')[1].startswith('i.'):
+                    img_url = img_url.split('//')[0] + '//i.' + img_url.split('//')[1]
+                if not img_url.endswith('.jpg'):
+                    img_url += '.jpg'
+                fname = img_url.split('/')[-1]
+                urllib.request.urlretrieve(img_url, os.path.join(dname, fname))
+        except Exception as e:
+            print(e)
+            
+            
+if __name__ == '__main__':
+    current_page = get_web_page(PTT_URL + '/bbs/Beauty/index.html')
+    if current_page:
+        articles = []  
+        date = time.strftime("%m/%d").lstrip('0')  
+        current_articles, prev_url = get_articles(current_page, date)  
+        
+        while current_articles:  
+            articles += current_articles
+            current_page = get_web_page(PTT_URL + prev_url)
+            current_articles, prev_url = get_articles(current_page, date)
+       
+        for article in articles:
+            page = get_web_page(PTT_URL + article['href'])
+            if page:
+                img_urls = parse(page)
+                save(img_urls, article['title'])
+                article['num_image'] = len(img_urls)  
+                
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(articles, f, indent=2, sort_keys=True, ensure_ascii=False)
